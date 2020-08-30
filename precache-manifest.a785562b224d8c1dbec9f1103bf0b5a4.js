407670f4da31950600d8c47f0e6f8d23a4ef9620
Paul <webdevpaulmason@gmail.com>
2020-08-29 18:05:05 -0700

    Updates

---
+++ precache-manifest.a785562b224d8c1dbec9f1103bf0b5a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfb383b5c8fe6bc0a09ea49e6b6bf1e5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c4b53e744fe5fef5983c",
+    "url": "/monsters-rolodex/static/css/main.1465a08e.chunk.css"
+  },
+  {
+    "revision": "4abf8657bdbe6955944f",
+    "url": "/monsters-rolodex/static/js/2.5941a3b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.5941a3b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4b53e744fe5fef5983c",
+    "url": "/monsters-rolodex/static/js/main.ec0fd8f0.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
